2b2a963efbbbd8948540b4e4dd7cc26f7d4f0f04
Luis Gabriel <luisgabrielprieto@gmail.com>
2024-02-07 22:25:57 -0500

    Commit message

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ import turtle as t
 from paddle import Paddle
 
 screen = t.Screen()
-screen.bgcolor("black")
+screen.bgcolor("white")
 screen.setup(width=600, height=600)
+screen.listen()
 
 paddle = Paddle()
+
+screen.onkey(paddle.move_up_paddle1,"w")
+screen.onkey(paddle.move_up_paddle2,"Up")
+screen.onkey(paddle.move_down_paddle1,"s")
+screen.onkey(paddle.move_down_paddle2,"Down")
+
+
+
 
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -5,12 +5,36 @@ class Paddle(t.Turtle):
 
     def __init__(self):
         super().__init__()
+        self.paddles = []
         self.paddle_body()
+        self.paddle1 = self.paddles[0]
+        self.paddle2 = self.paddles[1]
 
     def paddle_body(self):
-        for i in range (0,2):
+        for i in range(-1, 2, 2):
             paddle = t.Turtle()
+            paddle.penup()
+            paddle.speed("fastest")
+            paddle.goto(x=280 * i, y=0)
             paddle.shape("square")
             paddle.color("white")
             paddle.shapesize(stretch_len=0.5, stretch_wid=3, outline=1)
-            paddle.goto()
+            self.paddles.append(paddle)
+
+    def move_up_paddle1(self):
+
+        self.paddle1.setheading(90)
+        self.paddle1.forward(20)
+
+    def move_up_paddle2(self):
+
+        self.paddle2.setheading(90)
+        self.paddle2.forward(20)
+
+    def move_down_paddle1(self):
+        self.paddle1.setheading(270)
+        self.paddle1.forward(20)
+
+    def move_down_paddle2(self):
+        self.paddle2.setheading(270)
+        self.paddle2.forward(20)
